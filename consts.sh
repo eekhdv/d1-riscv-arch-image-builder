e286db95733c3c1cff17ec2aa55448f7ae8d7c12
Khadiev Edem <khadiev.edem@gmail.com>
2025-03-31 11:22:09 +0300

    feat: Update versions
    Update U-Boot version (from eekhdv repo)
    Update Archlinux rootfs version

--- consts.sh
+++ consts.sh
@@ -15,7 +15,7 @@ NPROC="$(nproc)"
 export PWD
 export NPROC
 
-export ROOT_FS='archriscv-2024-03-30.tar.zst'
+export ROOT_FS='archriscv-2024-09-22.tar.zst'
 export ROOT_FS_DL="https://archriscv.felixc.at/images/${ROOT_FS}"
 
 # select 'arch', 'defconfig'
@@ -45,7 +45,7 @@ export OUT_DIR="${PWD}/output"
 export SUDO='sudo'
 
 # use arch-chroot?
-export USE_CHROOT=1
+export USE_CHROOT=0
 
 # use extlinux ('extlinux') or boot.scr ('script') for loading the kernel?
 export BOOT_METHOD='extlinux'
@@ -54,14 +54,13 @@ export VERSION_OPENSBI='1.4'
 export VERSION_KERNEL='6.8'
 
 export SOURCE_OPENSBI="https://github.com/riscv-software-src/opensbi/releases/download/v${VERSION_OPENSBI}/opensbi-${VERSION_OPENSBI}-rv-bin.tar.xz"
-export SOURCE_UBOOT='https://github.com/smaeul/u-boot'
+export SOURCE_UBOOT='https://github.com/eekhdv/u-boot_sunxi_licheerv'
 export SOURCE_KERNEL="https://github.com/torvalds/linux/archive/refs/tags/v${VERSION_KERNEL}.tar.gz"
 export SOURCE_RTL8723='https://github.com/lwfinger/rtl8723ds.git'
 # https://github.com/karabek/xradio
 
 # pinned commits (no notice when things change)
-export COMMIT_UBOOT='329e94f16ff84f9cf9341f8dfdff7af1b1e6ee9a' # equals d1-2022-10-31
-export TAG_UBOOT='d1-2022-10-31'
+export COMMIT_UBOOT='70d23a02d8c8aa1de8d149ca930e7faf183e33ea' # equals d1-2022-10-31
 # use this (set to something != 0) to override the check
 export IGNORE_COMMITS=0
 export DEBUG='n'
